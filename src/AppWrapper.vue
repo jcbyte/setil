ef874cd60ee7a15e2e54e53819bfcb3ad182dcfd
Joel Cutler <joelcutler108@gmail.com>
2026-01-26 13:52:23 +0000

    change maintenance from middleware to client side rendering

---
+++ src/AppWrapper.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import App from "./App.vue";
+import MaintenancePage from "./pages/MaintenancePage.vue";
+
+const dev = import.meta.env.DEV;
+const maintenance = String(import.meta.env.VITE_MAINTENANCE ?? "false").toUpperCase() === "TRUE";
+const showMaintenance = maintenance && !dev;
+</script>
+
+<template>
+	<App v-if="!showMaintenance" />
+	<MaintenancePage v-else />
+</template>
---
+++ src/pages/MaintenancePage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Separator } from "@/components/ui/separator";
+const version = __APP_VERSION__;
+</script>
+
+<template>
+	<div class="flex justify-center items-center p-4">
+		<div class="flex flex-col items-center justify-center gap-3 border border-border p-8 rounded-lg min-w-80">
+			<div class="flex flex-col items-center justify-center">
+				<span class="text-2xl font-bold text-center">Setil will be back soon! 🚧</span>
+				<span class="text-muted-foreground text-center">Currently undergoing maintenance.</span>
+			</div>
+			<Separator />
+			<span class="text-sm text-muted-foreground text-center">Setil v{{ version }} by Joel Cutler</span>
+		</div>
+	</div>
+</template>
