63d7af11a0cec19c5192152c05e8ea8f82eb3ff8
Joel Cutler <joelcutler108@gmail.com>
2026-07-23 23:19:53 +0100

    let the client upload cloudinary image directly by returning a signature from api

--- src/cloudinary/types.ts
+++ src/cloudinary/types.ts
@@ -1,1 +1,9 @@
 export type AvatarUrl = string;
+
+export interface CloudinaryDetails {
+	signature: string;
+	timestamp: number;
+	cloudName: string;
+	apiKey: string;
+	uploadParams: { [key: string]: any };
+}
