a704d8f029761db4ddeddf02f44771e003df4e18
Joel Cutler <joelcutler108@gmail.com>
2026-07-25 23:55:46 +0100

    update invite page with new shadcn ui

--- src/pages/InvitePage.vue
+++ src/pages/InvitePage.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import Card from "@/components/ui/card/Card.vue";
+import CardContent from "@/components/ui/card/CardContent.vue";
 import { joinGroup } from "@/firebase/firestore/group";
 import { sendNotification } from "@/firebase/messaging";
-import { getRouteParam } from "@/util/util";
+import { getRouteParam } from "@/util/split";
 import { Loader } from "@lucide/vue";
 import { onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
@@ -46,10 +48,12 @@ onMounted(async () => {
 </script>
 
 <template>
-	<div class="flex items-center justify-center">
-		<div class="flex flex-col items-center justify-center gap-4 border border-border p-8 min-w-80 rounded-lg">
-			<Loader class="animate-spin !size-12" />
-			<span class="text-muted-foreground font-semibold">Validating Invite Link</span>
-		</div>
+	<div class="flex min-h-dvh items-center justify-center">
+		<Card class="-mt-16">
+			<CardContent class="flex flex-col items-center gap-4 p-8">
+				<Loader class="animate-spin !size-14" />
+				<span class="text-lg text-muted-foreground font-semibold">Validating Invite Link</span>
+			</CardContent>
+		</Card>
 	</div>
 </template>
