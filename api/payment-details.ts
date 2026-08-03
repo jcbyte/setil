94f2e51317372dc88c3df3511c9a6b460aacabed
Joel Cutler <joelcutler108@gmail.com>
2026-08-03 13:17:38 +0100

    use local imports for "@shared" on server-side to avoid node ESM treating it as a node module

--- api/payment-details.ts
+++ api/payment-details.ts
@@ -1,6 +1,6 @@
-import { PaymentDetailsPostBody } from "@shared/types/api.js";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { DocumentReference, getFirestore } from "firebase-admin/firestore";
+import { PaymentDetailsPostBody } from "../shared/types/api.js";
 import { PublicUserData } from "./_types/firestore.js";
 import { authenticateUser } from "./_utils/auth.js";
 import { decrypt, encrypt, EncryptedData } from "./_utils/crypt.js";
--- api/send-group-notification.ts
+++ api/send-group-notification.ts
@@ -1,8 +1,8 @@
-import { formatCurrency } from "@shared/currency.js";
-import type { SendGroupNotificationPostBody } from "@shared/types/api.js";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { DocumentReference, getFirestore } from "firebase-admin/firestore";
 import { FidMulticastMessage, getMessaging } from "firebase-admin/messaging";
+import { formatCurrency } from "../shared/currency.js";
+import type { SendGroupNotificationPostBody } from "../shared/types/api.js";
 import "./_init/firebaseAdmin.js";
 import type { GroupData, GroupUserData, PublicUserData, Transaction } from "./_types/firestore.js";
 import { authenticateUser } from "./_utils/auth.js";
