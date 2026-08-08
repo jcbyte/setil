031bc25dee4eecc5de12945b1edbb8b2773b0162
Joel Cutler <joelcutler108@gmail.com>
2026-08-08 19:29:46 +0100

    move google oauth web api client id to environment variable

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const app = createApp(AppWrapper);
 
 app.use(router);
 app.use(GoogleSignInPlugin, {
-	clientId: "913646123341-o4kstu61a8n1iiicq3i3olnc6h960u6u.apps.googleusercontent.com",
+	clientId: import.meta.env.VITE_GOOGLE_OAUTH_WEB_CLIENT_ID,
 });
 
 app.mount("#app");
