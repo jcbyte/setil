a1cf526cf4749bcb5c6c6433d74d651efd3733e6
Joel Cutler <joelcutler108@gmail.com>
2026-08-11 11:19:55 +0100

    create plugin for native navigation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from "vue";
 import GoogleSignInPlugin from "vue3-google-signin";
 import AppWrapper from "./AppWrapper.vue";
+import NativeNavigationPlugin from "./plugins/nativeNavigation.js";
 import NotificationPlugin from "./plugins/notifications.js";
 import router from "./router";
 import "./style.css";
-import { initialiseNativeNavigation } from "./util/app.js";
-
-initialiseNativeNavigation();
 
 const app = createApp(AppWrapper);
 
@@ -15,5 +13,6 @@ app.use(GoogleSignInPlugin, {
 	clientId: import.meta.env.VITE_GOOGLE_OAUTH_WEB_CLIENT_ID,
 });
 app.use(NotificationPlugin);
+app.use(NativeNavigationPlugin);
 
 app.mount("#app");
